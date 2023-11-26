db733363617225c997289b956d1b77908017cdf5
Yeremay <1yeremay1@gmail.com>
2023-11-26 21:09:09 +0000

    classes main and Person were implemented

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,44 @@
 public class Person {
+    private String Name;
+    private double Weight;
+    private double Height;
+
+    public Person(String name, double weight, double height) {
+        Name = name;
+        Weight = weight;
+        Height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "Name='" + Name + '\'' +
+                ", Weight=" + Weight +
+                ", Height=" + Height +
+                '}';
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public double getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(double weight) {
+        Weight = weight;
+    }
+
+    public double getHeight() {
+        return Height;
+    }
+
+    public void setHeight(double height) {
+        Height = height;
+    }
 }
