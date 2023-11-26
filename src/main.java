9494d44123d3dc8445873f23ae1caad522696960
Yeremay <1yeremay1@gmail.com>
2023-11-26 21:10:58 +0000

    Final

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
-
+        Person ejemplo = new Person("Yeremay",63.5,1.7);
+        System.out.println(ejemplo.getIMC());
     }
 }
